ef19a110ffe49ccb05914044c25982ee55ed3878
aman <amansaini1407@gmail.com>
2025-10-08 22:52:12 +0530

    Product Approved and Rejection Stages Tracker H

--- src/main/java/com/coder/springjwt/models/sellerModels/productModels/ProductDetailsModel.java
+++ src/main/java/com/coder/springjwt/models/sellerModels/productModels/ProductDetailsModel.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.List;
 
